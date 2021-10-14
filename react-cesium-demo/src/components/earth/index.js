7d5210ff7009efe47d0d6facd6681cffb013085d
乐子 <20988243+Guolefeng@users.noreply.github.com>
2021-10-14 14:10:18 +0800

    add terrain

--- react-cesium-demo/src/components/earth/index.js
+++ react-cesium-demo/src/components/earth/index.js
@@ -11,7 +11,12 @@ const Earth = () => {
             animation: false, // 是否创建动画小器件， 左下角仪表
             timeline: false, // 是否显示时间线控件
             fullscreenButton: false, // 右下角全屏按钮
+            terrainProvider: Cesium.createWorldTerrain({
+                requestVertexNormals: true,
+                requestWaterMask: true,
+            }),
         });
+        // viewer.scene.globe.enableLighting = true;
         viewer.camera.flyTo({
             destination: Cesium.Cartesian3.fromDegrees(116.39, 39.91, 20000000.0),
             // destination: Cesium.Cartesian3.fromDegrees(116.39, 39.91, 15000.0), // 天安门广场
