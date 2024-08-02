e5ae2e04e3da27c4d43e705b808a4887e8a69173
guolf <guolf@unis.com>
2024-08-02 14:11:39 +0800

    feat: 修改键盘kecode对应值

--- electron-demo/cat/preload.js
+++ electron-demo/cat/preload.js
@@ -7,45 +7,45 @@ let mouseX = 0;
 let mouseY = 0;
 
 window.addEventListener("DOMContentLoaded", () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector);
-    if (element) element.innerText = text;
-  };
+    const replaceText = (selector, text) => {
+        const element = document.getElementById(selector);
+        if (element) element.innerText = text;
+    };
 
-  for (const dependency of ["chrome", "node", "electron"]) {
-    replaceText(`${dependency}-version`, process.versions[dependency]);
-  }
-  // 设置拖拽功能
-  const el = document.getElementById("imgAvatar");
-  el.addEventListener("mousedown", (e) => {
-    dragging = true;
-    const { pageX, pageY } = e;
-    mouseX = pageX;
-    mouseY = pageY;
-  });
+    for (const dependency of ["chrome", "node", "electron"]) {
+        replaceText(`${dependency}-version`, process.versions[dependency]);
+    }
+    // 设置拖拽功能
+    const el = document.getElementById("imgAvatar");
+    el.addEventListener("mousedown", (e) => {
+        dragging = true;
+        const { pageX, pageY } = e;
+        mouseX = pageX;
+        mouseY = pageY;
+    });
 });
 // 鼠标抬起禁止拖拽
 window.addEventListener("mouseup", () => {
-  dragging = false;
+    dragging = false;
 });
 // 设置拖拽功能
 window.addEventListener("mousemove", (e) => {
-  console.log(dragging);
-  if (dragging) {
-    const { pageX, pageY } = e;
-    const pos = win.getPosition();
-    pos[0] = pos[0] + pageX - mouseX;
-    pos[1] = pos[1] + pageY - mouseY;
-    win.setPosition(pos[0], pos[1], true);
-  }
+    console.log(dragging);
+    if (dragging) {
+        const { pageX, pageY } = e;
+        const pos = win.getPosition();
+        pos[0] = pos[0] + pageX - mouseX;
+        pos[1] = pos[1] + pageY - mouseY;
+        win.setPosition(pos[0], pos[1], true);
+    }
 });
 // 设置指定区域的鼠标点击不穿透
 window.addEventListener("mousemove", (event) => {
-  const el = document.getElementById("imgAvatar");
-  let flag = event.target === el;
-  if (!flag) {
-    win.setIgnoreMouseEvents(true, { forward: true });
-  } else {
-    win.setIgnoreMouseEvents(false);
-  }
+    const el = document.getElementById("imgAvatar");
+    let flag = event.target === el;
+    if (!flag) {
+        win.setIgnoreMouseEvents(true, { forward: true });
+    } else {
+        win.setIgnoreMouseEvents(false);
+    }
 });
