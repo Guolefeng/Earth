19974dc454084ae4e347e717d568472f8d2579d2
guolf <guolf@unis.com>
2024-08-05 16:03:28 +0800

    feat: 修改cat左右脚动作

--- electron-demo/cat/src/renderer.js
+++ electron-demo/cat/src/renderer.js
@@ -2,20 +2,35 @@ const Flower = require("./src/Flower");
 let openSrc = "img/cat/body_open.png";
 let closeSrc = "img/cat/body_shut.png";
 const catbody = document.getElementById("catbody");
-// 爪子宽、高、距离猫身x/y间距
-const w = 30;
-const h = 20;
-const disX = 87;
-const disY = 90;
 const container = document.getElementById("zr");
+const zr = zrender.init(container);
 
+// 爪子宽、高、左右腿距离猫身x/y间距
+const w = 5;
+const h = 15;
+const leftDisX = 120;
+const leftDisY = 100;
+const rightDisX = 70;
+const rightDisY = 100;
 // 绘制猫爪
-const getPoints = (sp, ep, gap = 3) => {
+const getLeftPoints = (sp, ep, gap = 3) => {
+    return [
+        [sp.x - gap, sp.y - gap],
+        [sp.x - gap, sp.y + gap],
+        [sp.x + gap, sp.y + gap],
+        [ep.x + gap, ep.y + gap],
+        [ep.x + gap, ep.y - gap],
+        [ep.x - gap, ep.y - gap],
+    ];
+};
+
+const getRightPoints = (sp, ep, gap = 3) => {
     return [
         [sp.x - gap, sp.y + gap],
-        [sp.x - gap, sp.y - gap],
+        [sp.x + gap, sp.y + gap],
         [sp.x + gap, sp.y - gap],
         [ep.x + gap, ep.y - gap],
+        [ep.x - gap, ep.y - gap],
         [ep.x - gap, ep.y + gap],
     ];
 };
@@ -27,47 +42,95 @@ const getStartPoint = (e) => {
     };
 };
 
-let startPosition = {
-    x: container.clientWidth - w - disX,
-    y: container.clientHeight - h - disY,
+const getGradientColor = () => {
+    var gradient = new zrender.LinearGradient(0, 0, 1, 1);
+    gradient.addColorStop(0, "#8d8d8c");
+    gradient.addColorStop(1, "#000");
+    return gradient;
 };
-let endPosition = {
-    x: container.clientWidth - disX,
-    y: container.clientHeight - disY,
+
+// 左腿开始点位
+let leftLeg_sp = {
+    x: container.clientWidth - w - leftDisX,
+    y: container.clientHeight + h - leftDisY,
 };
-let defaultPoints = getPoints(startPosition, endPosition);
-var zr = zrender.init(container);
-var p = new zrender.Polygon({
+// 左腿结束点位
+let leftLeg_ep = {
+    x: container.clientWidth - leftDisX,
+    y: container.clientHeight - leftDisY,
+};
+// 左腿默认点位
+let leftLeg_dp = getLeftPoints(leftLeg_sp, leftLeg_ep);
+
+const leftLeg = new zrender.Polygon({
     shape: {
-        points: defaultPoints,
+        points: leftLeg_dp,
         smooth: 0.5,
     },
     style: {
-        fill: "#000",
+        fill: getGradientColor(),
     },
 });
-zr.add(p);
+zr.add(leftLeg);
+
+// 右腿开始点位
+let rightLeg_sp = {
+    x: container.clientWidth + w - rightDisX,
+    y: container.clientHeight + h - rightDisY,
+};
+// 右腿结束点位
+let rightLeg_ep = {
+    x: container.clientWidth - rightDisX,
+    y: container.clientHeight - rightDisY,
+};
+// 右腿默认点位
+let rightLeg_dp = getRightPoints(rightLeg_sp, rightLeg_ep);
+
+const rightLeg = new zrender.Polygon({
+    shape: {
+        points: rightLeg_dp,
+        smooth: 0.5,
+    },
+    style: {
+        fill: getGradientColor(),
+    },
+});
+zr.add(rightLeg);
 
 window.addEventListener("resize", () => {
     zr.resize();
-    startPosition = {
-        x: container.clientWidth - w - disX,
-        y: container.clientHeight - h - disY,
+    leftLeg_sp = {
+        x: container.clientWidth - w - leftDisX,
+        y: container.clientHeight + h - leftDisY,
     };
-    endPosition = {
-        x: container.clientWidth - disX,
-        y: container.clientHeight - disY,
+    leftLeg_ep = {
+        x: container.clientWidth - leftDisX,
+        y: container.clientHeight - leftDisY,
     };
-    let defaultPoints = getPoints(startPosition, endPosition);
-    p.attr({
+    leftLeg_dp = getLeftPoints(leftLeg_sp, leftLeg_ep);
+    leftLeg.attr({
         shape: {
-            points: defaultPoints,
+            points: leftLeg_dp,
+        },
+    });
+
+    rightLeg_sp = {
+        x: container.clientWidth - w - rightDisX,
+        y: container.clientHeight + h - rightDisY,
+    };
+    rightLeg_ep = {
+        x: container.clientWidth - rightDisX,
+        y: container.clientHeight - rightDisY,
+    };
+    rightLeg_dp = getRightPoints(rightLeg_sp, rightLeg_ep);
+    rightLeg.attr({
+        shape: {
+            points: rightLeg_dp,
         },
     });
 });
 
 // 绘制猫爪
-
 window.onGlobalKeyBoard = (type, keyCode) => {
     const ele = document.querySelector(`li[code='${keyCode}']`);
 
@@ -88,11 +151,30 @@ window.onGlobalKeyBoard = (type, keyCode) => {
         if (ele) {
             ele.classList.add("active");
             const sp = getStartPoint(ele);
-            p.attr({
-                shape: {
-                    points: getPoints(sp, endPosition),
-                },
-            });
+            if (ele.getAttribute("type") === "left") {
+                leftLeg.attr({
+                    shape: {
+                        points: getLeftPoints(sp, leftLeg_ep),
+                    },
+                });
+            } else if (ele.getAttribute("type") === "right") {
+                rightLeg.attr({
+                    shape: {
+                        points: getRightPoints(sp, rightLeg_ep),
+                    },
+                });
+            } else {
+                leftLeg.attr({
+                    shape: {
+                        points: getLeftPoints(sp, leftLeg_ep),
+                    },
+                });
+                rightLeg.attr({
+                    shape: {
+                        points: getRightPoints(sp, rightLeg_ep),
+                    },
+                });
+            }
         }
     }
 
@@ -105,11 +187,30 @@ window.onGlobalKeyBoard = (type, keyCode) => {
         // 键盘按钮同步 恢复默认状态
         if (ele) {
             ele.classList.remove("active");
-            p.attr({
-                shape: {
-                    points: defaultPoints,
-                },
-            });
+            if (ele.getAttribute("type") === "left") {
+                leftLeg.attr({
+                    shape: {
+                        points: leftLeg_dp,
+                    },
+                });
+            } else if (ele.getAttribute("type") === "right") {
+                rightLeg.attr({
+                    shape: {
+                        points: rightLeg_dp,
+                    },
+                });
+            } else {
+                leftLeg.attr({
+                    shape: {
+                        points: leftLeg_dp,
+                    },
+                });
+                rightLeg.attr({
+                    shape: {
+                        points: rightLeg_dp,
+                    },
+                });
+            }
         }
     }
 };
